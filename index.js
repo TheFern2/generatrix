4a3083a03af5cbfd5f7f1be8a783164aaca3c4b0
fredz0003 <ic3balandran@yahoo.com>
2019-07-02 18:56:54 -0500

    Beta test 2

--- index.js
+++ index.js
@@ -1,64 +1,123 @@
+var branchesArr = []
 
-window.onload=function(){
+class BranchModel {
+    constructor(branchName, sha, date) {
+        this._branchName = branchName
+        this._sha = sha
+        //this._date = date
+    }
 
-    var HttpClient = function() {
-        this.get = function(aUrl, aCallback) {
+    set updateDate(date) {
+        this._date = date
+    }
+}
+
+window.onload = function () {
+
+    var HttpClient = function () {
+        this.get = function (aUrl, aCallback) {
             var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function() { 
+            anHttpRequest.onreadystatechange = function () {
                 if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
                     aCallback(anHttpRequest.responseText);
             }
-    
-            anHttpRequest.open( "GET", aUrl, true );            
-            anHttpRequest.send( null );
+
+            anHttpRequest.open("GET", aUrl, true);
+            anHttpRequest.send(null);
         }
     }
 
-    function getBranchNames(jsonObj, repoName, repoOwner){
+    function getCommitDate(repoOwner, repoName, sha, index) {
+
+        // https://api.github.com/repos/:owner/:repo/git/commits/:commit_sha
+        var commitUrl = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/git/commits/" + sha
+        var client = new HttpClient()
+
+        client.get(commitUrl, function (response) {
+            // do something with response
+            //console.log(response);
+            var arr = JSON.parse(response)
+            console.log(arr.author.date)
+
+            branchesArr[index].updateDate = new Date(arr.author.date)
+
+        });
+
+    }
+
+    function branchSort(a, b) {
+        return a._date - b._date;
+    }
+
+    function getBranchNames(jsonObj, repoOwner, repoName) {
         var arr = JSON.parse(jsonObj)
         var outputStr = ""
 
-        for(var i = 0; i < arr.length; i++){
+        for (var i = 0; i < arr.length; i++) {
             //console.log(arr[i].ref)
             // split string
             var str = arr[i].ref
             var splitStrArr = str.split("/")
-            console.log(splitStrArr[splitStrArr.length-1])
-            var branchName = splitStrArr[splitStrArr.length-1]
+            console.log(splitStrArr[splitStrArr.length - 1])
+            var branchName = splitStrArr[splitStrArr.length - 1]
+            var sha = arr[i].object.sha
+            //date = getCommitDate(jsonObj, repoOwner, repoName, sha)
 
-            //console.log(arr[i].object.sha)
-            //- [Branch 1](https://www.w3schools.com/jsref/prop_textarea_value.asp)
+            // testing BranchModel object
+            branchesArr.push(new BranchModel(branchName, sha))
+            //branchesArr.push(new BranchModel(branchName, sha, date))
+            getCommitDate(repoOwner, repoName, sha, i)
+
+            //- [Branch 1](url to branch)
             outputStr += "- [" + branchName + "](https://github.com/" + repoOwner + "/" + repoName + "/tree/" + branchName + ")\n"
-            
+
         }
         console.log(outputStr)
         document.getElementById("markdown").value = outputStr
+
+        for (var k = 0; k < branchesArr.length; k++) {
+            console.log(branchesArr[k])
+        }
+
+        console.log("---------------------------------\n")
+
+        // testing sorting
+        branchesArr.sort(branchSort)
+
+        for (var k = 0; k < branchesArr.length; k++) {
+            console.log(branchesArr[k])
+        }
     }
 
     function getRefs() {
         var repoOwner = document.getElementById("repoOwner").value;
         var repoName = document.getElementById("repoName").value;
+
+        // testing values
+        repoOwner = "kodaman2"
+        repoName = "TTT-Book"
+
         var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/git/refs"
         //var url = "https://api.github.com/repos/kodaman2/Data_Preserve/git/refs"
         //alert("I am an alert box!");
         var client = new HttpClient();
-        
-        client.get(url, function(response) {
-        // do something with response
-        console.log(response);
 
-        getBranchNames(response, repoName, repoOwner)
+        client.get(url, function (response) {
+            // do something with response
+            console.log(response);
 
-         // need to create an object array
-         // with branch name, and sha, and date
+            getBranchNames(response, repoOwner, repoName)
 
-         //then checks commit date with sha, update dates
+            // need to create an object array
+            // with branch name, and sha, and date
 
-         // lastly sort by date
+            //then checks commit date with sha, update dates
+
+            // lastly sort by date
         });
-      }
+    }
 
-    function main(){
+    function main() {
         getRefs()
     }
 
